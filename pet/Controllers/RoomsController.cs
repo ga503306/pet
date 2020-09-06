599878f45d0eb4b9cc5ded83285f76e9b01893ad
hzt <ga503306@yahoo.com.tw>
2020-09-07 01:28:52 +0800

    產品api - getcompanys

---
+++ pet/Controllers/RoomsController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web.Http;
+using System.Web.Http.Description;
+using pet.Filter;
+using pet.Models;
+using pet.Security;
+using WebApplication1.Models;
+
+namespace pet.Controllers
+{
+    [RoutePrefix("api/Room")]
+    public class RoomsController : ApiController
+    {
+        private Model1 db = new Model1();
+        // Get: api/Room/GetCompanys //前台拿所有廠商 未被封鎖的
+        [JwtAuthFilter]
+        [Route("GetCompanys")]
+        [HttpGet]
+        public IHttpActionResult GetCompanys()
+        {
+            List<RoomCompanyModel> roomCompanyModels = new List<RoomCompanyModel>();
+            List<Company> companies = db.Company.Where(x => x.del_flag == "N").ToList();
+            foreach (Company company in companies)
+            {
+                RoomCompanyModel roomCompanyModel = new RoomCompanyModel();
+                roomCompanyModel.companyseq = company.companyseq;
+                roomCompanyModel.companybrand = company.companybrand;
+                roomCompanyModel.avatar = company.avatar;
+                roomCompanyModel.country = company.country;
+                roomCompanyModel.area = company.area;
+                roomCompanyModel.address = company.address;
+                List<Room> rooms = db.Room.Where(x => x.companyseq == company.companyseq &&
+                                                     x.state == Roomstate.已上架 &&
+                                                     x.del_flag == "N").ToList();
+                //廠商有 (已上架 && 未刪除) 的寄宿空間
+                if (rooms.Count > 0)
+                {
+                    bool hascat = false;
+                    bool hasdog = false;
+                    bool hasother = false;
+                    int price_min = int.MaxValue;
+                    int price_max = int.MinValue;
+                    foreach (Room r in rooms)
+                    {
+                        //如果某一間房間有勾貓 就在廠商顯示貓總類
+                        if (r.pettype_cat.Value)
+                            hascat = true;
+                        if (r.pettype_dog.Value)
+                            hasdog = true;
+                        if (r.pettype_other.Value)
+                            hasother = true;
+                        //紀錄最大金額 最小金額
+                        if (r.roomprice > price_max)
+                            price_max = r.roomprice.Value;
+                        if (r.roomprice < price_min)
+                            price_min = r.roomprice.Value;
+                    }
+                    if (hascat)
+                        roomCompanyModel.pettype += "貓咪 ";
+                    if (hasdog)
+                        roomCompanyModel.pettype += "狗 ";
+                    if (hasother)
+                        roomCompanyModel.pettype += "其他 ";
+
+                    roomCompanyModel.roomprice_min = price_min;
+                    roomCompanyModel.roomprice_max = price_max;
+                }
+                roomCompanyModel.rooms = rooms is null ? 0 : rooms.Count;
+                roomCompanyModels.Add(roomCompanyModel);
+            }
+
+            return Ok(roomCompanyModels);
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RoomExists(string id)
+        {
+            return db.Room.Count(e => e.roomseq == id) > 0;
+        }
+    }
+}
