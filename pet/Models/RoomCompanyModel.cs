688be1ba360670d8faab12425edc69e53810e351
hzt <ga503306@yahoo.com.tw>
2020-09-07 01:23:44 +0800

    產品用的viewmodel

---
+++ pet/Models/RoomCompanyModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pet.Models
+{
+    public class RoomCompanyModel
+    {
+        public string companyseq { get; set; }
+
+        public string companybrand { get; set; }
+
+        public string avatar { get; set; }
+
+        public string country { get; set; }
+
+        public string area { get; set; }
+
+        public string address { get; set; }
+
+        public string pettype { get; set; }
+
+        public int? roomprice_min { get; set; }
+
+        public int? roomprice_max { get; set; }
+
+        public int? rooms { get; set; }
+    }
+}
---
+++ pet/Models/RoomModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pet.Models
+{
+    public class RoomModel
+    {
+        public string companyseq { get; set; }
+
+        public string companybrand { get; set; }
+
+        public string avatar { get; set; }
+
+        public string country { get; set; }
+
+        public string area { get; set; }
+
+        public string address { get; set; }
+
+        public string pettype { get; set; }
+
+        public int? roomprice { get; set; }
+
+    }
+}
