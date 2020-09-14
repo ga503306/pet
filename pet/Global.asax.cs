891fd69fce31284ca3bdf82ffd654eaf52a18a72
hzt <ga503306@yahoo.com.tw>
2020-09-14 21:21:28 +0800

    排程 完成訂單 日期<今日的單 要改狀態完成

--- pet/Global.asax.cs
+++ pet/Global.asax.cs
@@ -1,6 +1,9 @@
 using pet.Filter;
+using pet.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -12,6 +15,10 @@ namespace pet
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        public System.ComponentModel.IContainer components = null;
+        public System.Timers.Timer timer1;
+        private Model1 db = new Model1();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -19,6 +26,7 @@ namespace pet
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            InitializeComponent();
         }
         protected void Application_BeginRequest()
         {
@@ -27,5 +35,33 @@ namespace pet
                 Response.End();
             }
         }
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Timers.Timer();
+            timer1.Interval = 28800000;// 每10分鐘執行一次
+            timer1.Enabled = true;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(timer1_Elapsed);
+        }
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ThreadStart(Backup));
+            thread.Start();
+        }
+
+        void Backup()
+        {
+            List<Order> order = db.Order.Where(x => x.orderdatee < DateTime.Now && x.state == 1).ToList();
+            foreach (Order o in order)
+            {
+                o.state = (int)Orderstate.已完成;
+                db.Entry(o).State = EntityState.Modified;
+            }
+            if (order != null)
+            {
+                db.SaveChanges();
+            }
+        }
+
     }
 }
