dc59270accaa0d1f00d4d62cb46c090ec13272e8
hzt <ga503306@yahoo.com.tw>
2020-09-17 01:54:44 +0800

    測試api

---
+++ pet/Controllers/TestController.cs
@@ -0,0 +1,52 @@
+﻿using pet.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace pet.Controllers
+{
+    [RoutePrefix("api/Test")]
+    public class TestController : ApiController
+    {
+        private Model1 db = new Model1();
+
+        // Get: api/Test/OrderState //修改訂單狀態
+        [Route("OrderState")]
+        [HttpPost]
+        public IHttpActionResult OrderState_(OrderState orderstate)
+        {
+            if (orderstate.orderseq != null)
+            {
+                Order order = db.Order.Find(orderstate.orderseq);
+                order.state = orderstate.state;
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            else
+            {
+                List<Order> orders = db.Order.Where(x => x.state == (int)Orderstate.已取消).ToList();
+                foreach (Order o in orders)
+                {
+                    o.state = orderstate.state;
+                    db.Entry(o).State = EntityState.Modified;
+                }
+
+                db.SaveChanges();
+            }
+            return Ok(new
+            {
+                result = "修改成功"
+            });
+        }
+
+        public class OrderState
+        {
+            public string orderseq { get; set; }
+            public int state { get; set; }
+        }
+    }
+}
